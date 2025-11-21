4d438a5cc8cd84099224d34b28f1062a1bbf3bff
OriolGarciaOrtiz <oriol.garcia.ortiz@estudiantat.upc.edu>
2025-11-21 23:53:42 +0100

    MQTT dron

--- TFG/MQTT Program dron/HexsoonClass.py
+++ TFG/MQTT Program dron/HexsoonClass.py
@@ -1,5 +1,7 @@
 from dronLink.Dron import Dron
 from ultralytics import YOLO
+from pymavlink import mavutil
+from dronLink.modules.dron_move import _prepare_command_mov
 
 
 class HexsoonController:
@@ -13,3 +15,110 @@ class HexsoonController:
         except Exception as e:
             print("Could not load YOLO model:", e)
             self.model = None
+
+        self.integral_x
+        self.integral_y
+
+
+    def connect_drone(self, mode: str):
+
+        try:
+
+            if mode == "Simulation":
+                self.dron.connect('tcp:127.0.0.1:5763', 115200)
+
+            elif mode == "Practice": 
+                self.dron.connect('COM3', 57600)
+
+            else: 
+                raise ValueError("Unknown connection mode selected")
+            
+            self.is_connected = True
+
+            if not self.cap.isOpened():
+                raise Exception("Could not access local camera")
+            
+        except:
+            self.is_connected = False
+
+
+    def disconnect_drone(self): 
+        if self.is_connected:
+            self.dron.disconnect()   
+
+
+    def take_off_drone(self, alt = 3):
+        if not self.is_connected:
+            return
+        
+        try:
+            alt = int(alt)
+            if alt <= 0:
+                return
+            
+        except:
+            pass
+
+
+    def land_drone(self):
+        if self.is_connected:
+            self.dron.Land()
+
+
+    def Return_To_Launch(self):
+        if self.is_connected:
+            self.dron.RTL()
+
+
+    def arm(self):
+        if self.is_connected:
+            self.dron.arm()
+
+
+    def set_velocity(self, left_right, for_back, up_down, yaw=0):
+
+        try:
+
+            vehicle: mavutil.mavfile = getattr(self.dron, "vehicle", None)
+
+            if vehicle is None:
+                return
+
+            step_x = for_back / 100.0
+            step_y = left_right / 100.0
+            step_z = -up_down / 100.0
+
+            msg = _prepare_command_mov(self.dron, step_x, step_y, step_z, bodyRef=True)
+            print(msg)
+            vehicle.mav.send(msg)
+
+        except:
+            return
+        
+
+    def set_param(self, name, value):
+
+        # El motivo de esta función es la siguiente. El problema era que al usar _prepare_command_mov a partir de 0.4 m/s de velocidad en
+        # left right el dron empezaba a girar en yaw. Intenté hacer _prepare_command_mov_changed con el objetivo de cambiar la máscara para solo velcoidades
+        # y que el movimiento no fuera por posición sino por velocidad pero segía moviendose en yaw el dron. Después pen´se que podría ser
+        # a causa de que estaba cogiendo la referencia del dron pero cambiadba a la NED pero tampoco era eso. Así que como ñultima opción me quedó
+        # que fuera un problema del guided mode. Esto por lo que se ve sucede por un bug / comportamiento por diseño del controlador de GUIDED de ArduCopter.
+        # En el firmware ArduCopter cuando se envia un mensaje SET_POSITION_TARGET_LOCAL_NED con velocidades laterales (vy) en BODY_NED,el controlador interno asume que estás pidiendo
+        # “moverte lateralmente respecto al rumbo actual”.Pero si el yaw no está bloqueado o el modo GUIDED no está limitado, el autopiloto interpreta el movimiento lateral como una
+        # instrucción de girar el yaw para “alinearse” con el vector de velocidad, es decir, ArduCopter intenta mirar hacia donde te mueves.
+        # Por ello he hecho esta función que mantiene el headind fijo y bloquea el yaw en modo guided cambiando las opciones de modo 0 a modo 8   
+
+        vehicle: mavutil.mavfile = getattr(self.dron, "vehicle", None)
+        msg = mavutil.mavlink.MAVLink_param_set_message(
+            vehicle.target_system,
+            vehicle.target_component,
+            name.encode("utf-8"),
+            float(value),
+            mavutil.mavlink.MAV_PARAM_TYPE_REAL32
+        )
+        vehicle.mav.send(msg)
+
+
+    def stabilizeYaw(self):
+        self.set_param("WP_YAW_BEHAVIOR", 0)  # Mantener heading fijo
+        self.set_param("GUID_OPTIONS", 8)  # Bloquear yaw en GUIDED
--- TFG/MQTT Program dron/HexsoonGUI.py
+++ TFG/MQTT Program dron/HexsoonGUI.py
@@ -1,18 +1,6 @@
 from HexsoonClass import *
-import pygetwindow as gw
-import mss
-from pymavlink import mavutil
-from dronLink.modules.dron_move import _prepare_command_mov
-import win32gui
-import win32ui
-import win32con
-import win32api
-import ctypes
 import cv2
 import numpy as np
-import tkinter as tk
-from tkinter import Scale, messagebox, OptionMenu, Label, StringVar
-from PIL import Image, ImageTk
 import threading
 
 
@@ -24,8 +12,47 @@ class GUI:
         self.panel_width = 320
         self.panel_height = 240
 
-    #Helper para ejecutar acciones en segundo plano y que el tk no se quede pillado en las funciones de MavLink
+        '''  Todos los parametros que hay que enviar a esta aplicación  '''
+
+        self.h_min
+        self.h_max
+        self.s_min
+        self.s_max
+        self.v_min
+        self.v_max
+
+        self.detection_mode
+
+        self.Kp_x
+        self.Ki_x
+        self.Kd_x
+        self.Kp_y
+        self.Ki_y
+        self.Kd_y
+
+        self.PID_mode
+
+        self.max_velocity
+
+        self.view_mode
+
+
+    def recieve_data(self): 
+        pass
+
+    def set_data(self):
+        pass
+
+    def prepare_data(self):
+        pass
+
+    def send_data(self):
+        pass
+
+    
     def run_in_thread(self, target, *args, status_msg="Executing..."):
+
+        #Helper para ejecutar acciones en segundo plano y que el tk no se quede pillado en las funciones de MavLink
 
         def task():
             try:
@@ -37,143 +64,10 @@ class GUI:
 
         threading.Thread(target=task, daemon=True).start()
 
-    # El motivo de esta función es la siguiente. El problema era que al usar _prepare_command_mov a partir de 0.4 m/s de velocidad en
-    # left right el dron empezaba a girar en yaw. Intenté hacer _prepare_command_mov_changed con el objetivo de cambiar la máscara para solo velcoidades
-    # y que el movimiento no fuera por posición sino por velocidad pero segía moviendose en yaw el dron. Después pen´se que podría ser
-    # a causa de que estaba cogiendo la referencia del dron pero cambiadba a la NED pero tampoco era eso. Así que como ñultima opción me quedó
-    # que fuera un problema del guided mode. Esto por lo que se ve sucede por un bug / comportamiento por diseño del controlador de GUIDED de ArduCopter.
-    # En el firmware ArduCopter cuando se envia un mensaje SET_POSITION_TARGET_LOCAL_NED con velocidades laterales (vy) en BODY_NED,el controlador interno asume que estás pidiendo
-    # “moverte lateralmente respecto al rumbo actual”.Pero si el yaw no está bloqueado o el modo GUIDED no está limitado, el autopiloto interpreta el movimiento lateral como una
-    # instrucción de girar el yaw para “alinearse” con el vector de velocidad, es decir, ArduCopter intenta mirar hacia donde te mueves.
-    # Por ello he hecho esta función que mantiene el headind fijo y bloquea el yaw en modo guided cambiando las opciones de modo 0 a modo 8   
-
-    def stabilizeYaw(self):
-        self.set_param("WP_YAW_BEHAVIOR", 0)  # Mantener heading fijo
-        self.set_param("GUID_OPTIONS", 8)  # Bloquear yaw en GUIDED
-
-    def set_param(self, name, value):
-        dron = self.controller.dron  # tipo: Dron
-        vehicle: mavutil.mavfile = getattr(dron, "vehicle", None)
-        msg = mavutil.mavlink.MAVLink_param_set_message(
-            vehicle.target_system,
-            vehicle.target_component,
-            name.encode("utf-8"),
-            float(value),
-            mavutil.mavlink.MAV_PARAM_TYPE_REAL32
-        )
-        vehicle.mav.send(msg)
-
-    def connect(self):
-        try:
-            mode = self.simulation_var.get()
-
-            if mode == "Simulation":
-                self.log("Connecting to simulated drone (Mission Planner TCP)...")
-                self.controller.dron.connect('tcp:127.0.0.1:5763', 115200)
-            elif mode == "Practice":
-                self.log("Connecting to real drone on COM3...")
-                self.controller.dron.connect('COM3', 57600)
-            else:
-                raise ValueError("Unknown connection mode selected")
-
-            self.is_connected = True
-            self.connected_label.config(text="✅ Connected", fg="green")
-
-            self.log("Configuring yaw lock (stabilizeYaw)...")
-            self.stabilizeYaw()
-            self.log("Yaw stabilized (no automatic rotation).")
-
-
-            self.controller.cap = cv2.VideoCapture(0)
-            if not self.controller.cap.isOpened():
-                raise Exception("Could not access local camera")
-
-            self.log("Connection successful")
-
-            self.update_frame()
-
-        except Exception as e:
-            self.log(f"Connection error: {e}")
-            self.is_connected = False
-            self.connected_label.config(text="❌ Not Connected", fg="red")
-
-    def disconnect(self):
-        if self.is_connected:
-            self.controller.dron.disconnect()
-        self.log("🔌 Disconnected from drone and camera")
-
-
-
-    def takeoff(self, alt=3):
-        if not self.is_connected:
-            self.log("Drone is not connected. Cannot take off.")
-            tk.messagebox.showwarning("Warning", "Connect to the drone before takeoff.")
-            return
-
-        try:
-            alt = int(alt)
-            if alt <= 0:
-                raise ValueError("Altitude must be greater than 0")
-
-            self.controller.dron.takeOff(alt)
-            self.log(f"Taking off to {alt} meters")
-
-        except ValueError:
-            self.log("Invalid altitude value")
-            tk.messagebox.showerror("Error", "Please enter a valid numeric altitude (e.g., 5 or 10).")
-
-    def land(self):
-        if self.is_connected:
-            self.controller.dron.Land()
-            self.log("Landing...")
-    def Return_To_Launch(self):
-        if self.is_connected:
-            self.controller.dron.RTL()
-
-    def arm(self):
-        if self.is_connected:
-            self.controller.dron.arm()
-            self.log("Drone armed")
-
-    def log(self, msg):
-        self.root.after(0, lambda: self.status_var.set(msg))
-
-
-    @staticmethod
-    def safe_print_window(hwnd, hdc, flags):
-        user32 = ctypes.windll.user32
-        user32.PrintWindow.restype = ctypes.c_bool
-        return user32.PrintWindow(hwnd, hdc, flags)
-
-
-    def set_velocity(self, left_right, for_back, up_down, yaw=0):
-        try:
-            dron = self.controller.dron  # tipo: Dron
-            vehicle: mavutil.mavfile = getattr(dron, "vehicle", None)
-            if vehicle is None:
-                self.log("No active connection with the drone (vehicle = None)")
-                return
-
-            step_x = for_back / 100.0
-            step_y = left_right / 100.0
-            step_z = -up_down / 100.0
-            #Def de dronLink. Se supone que en mavlink  stepx seria la velocidad hacia al norte
-            #(for back si bodyRef = True), set_y seria hacia el este (left-right si bodyRef = True) y set_z sería la up down (bodyRef = True). ¡
-
-            #Problema, a veces para velocidades altas hace giros de yaw
-            msg = _prepare_command_mov(dron, step_x, step_y, step_z, bodyRef=True)
-            self.log(str(msg))
-            print(msg)
-            vehicle.mav.send(msg)
-
-            #self.log(f"Sent speeds: X={step_x:.2f}, Y={step_y:.2f}, Z={step_z:.2f}")
-
-        except Exception as e:
-            self.log(f"Error sending speeds to the drone: {e}")
 
     def update_frame(self):
         try:
-            if not self.is_connected or not self.controller.cap or not self.controller.cap.isOpened():
+            if not self.controller.is_connected or not self.controller.cap or not self.controller.cap.isOpened():
                 self.log("Camera not initialized or drone not connected.")
                 return
 
@@ -186,8 +80,8 @@ class GUI:
             frame_display = cv2.resize(frame, (self.panel_width, self.panel_height))
             frame_hsv = cv2.cvtColor(frame_display, cv2.COLOR_BGR2HSV)
 
-            lower = np.array([self.h_min.get(), self.s_min.get(), self.v_min.get()])
-            upper = np.array([self.h_max.get(), self.s_max.get(), self.v_max.get()])
+            lower = np.array([self.h_min, self.s_min, self.v_min])
+            upper = np.array([self.h_max, self.s_max, self.v_max])
             mask = cv2.inRange(frame_hsv, lower, upper)
             result = cv2.bitwise_and(frame_display, frame_display, mask=mask)
 
@@ -196,12 +90,12 @@ class GUI:
             img_canny = cv2.Canny(img_gray, self.t1.get(), self.t2.get())
             kernel = np.ones((5, 5), np.uint8)
             img_dil = cv2.dilate(img_canny, kernel, iterations=1)
+
             object_center = None
             x, y, w, h = 0, 0, 0, 0
-            detection_mode = self.detection_var.get()
             img_contour = frame_display.copy()
 
-            if detection_mode == "Color Contour":
+            if self.detection_mode == "Color Contour":
                 img_blur = cv2.GaussianBlur(result, (7, 7), 1)
                 img_gray = cv2.cvtColor(img_blur, cv2.COLOR_BGR2GRAY)
                 img_canny = cv2.Canny(img_gray, self.t1.get(), self.t2.get())
@@ -218,7 +112,7 @@ class GUI:
                         cv2.circle(img_contour, object_center, 5, (0, 0, 255), cv2.FILLED)
 
 
-            elif detection_mode == "Neural Network" and self.controller.model is not None:
+            elif self.detection_mode == "Neural Network" and self.controller.model is not None:
                 #results = self.controller.model.predict(frame_display, conf=0.5, verbose=False)
                 #track tiene el objetivo de mantiene los IDs de objetos entre frames. Usa menos recursos si el objeto no se mueve mucho
                 #así que quizas puede mejorar la velocidad para que la camara no vaya con tanto delay
@@ -254,9 +148,10 @@ class GUI:
             if object_center is not None:
                 cx, cy = object_center
                 error_x = cx - (self.panel_width / 2)
-                if self.opt_cam.get() == "Front View":
+
+                if self.view_mode == "Front View":
                     error_y = (self.panel_height / 2) - cy
-                else:  # Bottom View
+                else:  
                     error_y = cy - (self.panel_height / 2)
 
                 self.controller.integral_x = getattr(self.controller, "integral_x", 0) + error_x
@@ -264,55 +159,46 @@ class GUI:
                 derivative_x = error_x - getattr(self.controller, "prev_error_x", 0)
                 derivative_y = error_y - getattr(self.controller, "prev_error_y", 0)
 
-                Kpx, Kix, Kdx = self.Kp_x.get(), self.Ki_x.get(), self.Kd_x.get()
-                Kpy, Kiy, Kdy = self.Kp_y.get(), self.Ki_y.get(), self.Kd_y.get()
+                Kpx, Kix, Kdx = self.Kp_x, self.Ki_x, self.Kd_x
+                Kpy, Kiy, Kdy = self.Kp_y, self.Ki_y, self.Kd_y
 
-                mode_ctrl = self.opt.get()
-                if mode_ctrl == "P":
+                if self.PID_mode == "P":
                     vx = Kpx * error_x
                     vy = Kpy * error_y
-                elif mode_ctrl == "I":
+                elif self.PID_mode == "I":
                     vx = Kix * self.controller.integral_x
                     vy = Kiy * self.controller.integral_y
-                elif mode_ctrl == "D":
+                elif self.PID_mode == "D":
                     vx = Kdx * derivative_x
                     vy = Kdy * derivative_y
-                elif mode_ctrl == "PI":
+                elif self.PID_mode == "PI":
                     vx = Kpx * error_x + Kix * self.controller.integral_x
                     vy = Kpy * error_y + Kiy * self.controller.integral_y
-                elif mode_ctrl == "PD":
+                elif self.PID_mode == "PD":
                     vx = Kpx * error_x + Kdx * derivative_x
                     vy = Kpy * error_y + Kdy * derivative_y
-                elif mode_ctrl == "PID":
+                elif self.PID_mode == "PID":
                     vx = Kpx * error_x + Kix * self.controller.integral_x + Kdx * derivative_x
                     vy = Kpy * error_y + Kiy * self.controller.integral_y + Kdy * derivative_y
                 else:
                     vx, vy = error_x, error_y
 
-                vx = int(np.clip(vx, -self.max_velocity.get(), self.max_velocity.get()))
-                vy = int(np.clip(vy, -self.max_velocity.get(), self.max_velocity.get()))
+                vx = int(np.clip(vx, -self.max_velocity, self.max_velocity))
+                vy = int(np.clip(vy, -self.max_velocity, self.max_velocity))
 
                 self.controller.prev_error_x = error_x
                 self.controller.prev_error_y = error_y
 
-                view_mode = self.opt_cam.get()  # Front View o Bottom View
-                if view_mode == "Front View":
+                if self.view_mode == "Front View":
                     left_right = vx
                     up_down = vy
                     for_back = 0
-                else:  # Bottom View
+                else:
                     left_right = vx
                     for_back = vy
                     up_down = 0
 
-                # Funciona pero se le pira la cabeza con la left right cuando la velocidad es más garnde a 40
-                self.set_velocity(left_right, for_back, up_down, yaw=0)
-
-            mission_frame = self.get_mission_planner_frame()
-            if mission_frame is None:
-                mission_frame = np.zeros((480, 360, 3), dtype=np.uint8)
-                cv2.putText(mission_frame, "Mission Planner not found",
-                            (20, 120), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+                self.controller.set_velocity(left_right, for_back, up_down, yaw=0)
 
 
         except Exception as e:
