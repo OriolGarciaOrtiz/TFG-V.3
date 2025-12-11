87bce7e5328343fc5a040a1383d9c102f4867220
OriolGarciaOrtiz <oriol.garcia.ortiz@estudiantat.upc.edu>
2025-12-11 19:56:50 +0100

    Cambios pre test

--- TFG/MQTT Program dron/main.py
+++ TFG/MQTT Program dron/main.py
@@ -1,24 +1,36 @@
 from HexsoonGUI import GUI as HexsoonGUI
 import time
+import asyncio
+import threading 
 
 class DroneLauncher:
-    def __init__(self):
-        self.drone_gui = HexsoonGUI()
-    
+    def __init__(self):    
+                    
+        # Thread para WebRTC
+        self.loop = asyncio.new_event_loop()
+        self.thread = threading.Thread(target=self._run_loop, daemon=True)
+        self.thread.start()
+        
+        self.drone_gui = HexsoonGUI(self.loop)
+        
+    def _run_loop(self):
+        asyncio.set_event_loop(self.loop)
+        self.loop.run_forever()
+
     def main(self):
         try:
-
-            while True: 
+            while True:
+                
                 self.drone_gui.update_frame()
+
                 time.sleep(0.01)
 
         except Exception as e:
             raise e
-
-
-
+          
+          
 if __name__ == "__main__":
 
-    launcher = DroneLauncher()
+    dron = DroneLauncher()
+    dron.main()
 
-    launcher.main()
